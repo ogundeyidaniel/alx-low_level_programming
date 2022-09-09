5fe7397a96c6f073ed8907d45051cf8cf077fa9a
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-09 12:46:42 -0700

    Task 0

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -16,8 +16,7 @@ int main(void)
 	q = 'q';
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != e && low != q)
-		        putchar(low);
+		if (low != e && low != q)											   putchar(low);
 	}
 	putchar('\n');
 
