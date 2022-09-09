c586d8c621946dbe8ded77ce5587fa066d70a6b9
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-09 14:06:50 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Write a program that prints all single digit numbers of base 10 starting from 0.
+ * main - Write a program that prints all single digit numbers of base 10.
  * All your code should be in the main function
  * Return: Always 0 (Success)
  */
