9fc0309996563f93e71da5521cd6847458da9f51
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-09 08:52:08 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Prints numbers from 0 to 9.
@@ -9,10 +11,11 @@ int main(void)
 {
 	int i;
 	
-	for (i + o; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
 	putchar('\n');
+	
 	return (0);
 }
