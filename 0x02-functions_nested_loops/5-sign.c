7eecb5c67b7574f5800ecf47a4c83ac27344692c
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 07:11:14 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  print_sign - Dtermines if the input variable is greater,
+ *  equal or less than zero
+ *
+ *  @n: The input variable as an interger.
+ *
+ *  Return: 1 and prints + if n is greater than zero
+ *  0 and prints 0 if n is zero
+ *  -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0);
+	{
+		_putchar(45);
+		return(-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
