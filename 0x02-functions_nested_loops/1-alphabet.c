8ab67c12114364e0da9708bd940191e5d2c7195b
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 06:37:08 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_alphabet - Prints the alphabet in lowercase.
@@ -7,13 +7,11 @@
  */
 void print_alphabet(void)
 {
-	char i;
+	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	
-	return (0);
 }
