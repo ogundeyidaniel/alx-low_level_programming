0b90013ac3f138181b0539b2e105896719a819ca
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 05:32:12 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Shows 1 if the input is a
+ * show 0 for another letter cases
+ * Return: 1 for letters. 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
