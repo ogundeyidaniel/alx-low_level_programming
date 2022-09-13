00cd0e2ee2712b44885ca896f1d595f22590f3c1
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 10:24:35 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,10 @@
 /**
  * add - A function that adds two integers and
  * returns the result.
+ *
  * @q: An integer a
  * @b: An integer b
+ *
  * Return: Summation of a and b.
  */
 int add(int a, int b)
