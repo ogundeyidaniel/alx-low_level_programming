1cc72d1f533c3ce97688bedf3c4189de9b694ed3
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 03:34:07 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ *  main - write a program thats prints _putchar
+ *
+ *
+ *  Return: Success 0
+ */
+int _putchar(char c)
+{ 
+	return (write(1, &c, 1);
+}
