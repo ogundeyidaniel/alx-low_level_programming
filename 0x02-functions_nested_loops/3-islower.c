20b6143ff99789aaebd085afcc8a8f2a90c2ff7f
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 04:20:25 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * _islower - Shows 1 if c is lowercase
+ * Return 0 otherwise
+ */
+
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122
+	{
+	       return (1);
+	}
+	else
+	{
+	       return (0);
+	}
+	_putchar('\n');
+}
