458ed7e8d4845ab8aebeb6628eb36ac5a12d7a1b
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 09:10:15 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - a function that prints every minute of 
- * the day of Jack Bauer, starting from 00:00 to 23:59.
+ * the day of Jack Bauer,starting from 00:00 to 23:59.
  *
  * Return: no return
  */
