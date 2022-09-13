394d2945149e0da38cbdae60fd61b9ac2f4bc6da
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-13 03:59:20 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - prints 10 times the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 122; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
+}
