6e2c457b842feab2b81a0a63c3df0a3ce6708398
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-14 05:52:49 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,14 +7,14 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
- * Return: Nothing
+ * Return: Void
  */
 
 void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || (year % 400 == 0 && !(year % 100 == 0)))
 	{
-		if (month >= 2)
+		if (month >= 2 && day == 60)
 		{
 			day++;
 		}
