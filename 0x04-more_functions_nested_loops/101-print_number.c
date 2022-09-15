43fb3defed7e6a06747212451741402c2e4ec738
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-15 14:43:09 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,18 +6,55 @@
  * @n: number to be printed
  * Return:void
  */
-
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int un = 0;
 
-	x = n;
 	if (n < 0)
 	{
+		if (n < 1000000000)
+			n = -n;
+		un = n;
 		_putchar(45);
-		x = -n;
+		_putchar(45);
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	else
+	{
+		un = n;
+		num_to_char(un);
+	}
+}
+
+/**
+ *  num_to_char - transforms a number with 1 or more digits into a char num_to_char
+ *  @n: number to print
+ *
+ *  Return: void
+ */
+void num_to_char(unsigned int n)
+{
+	unsigned int d = 10;
+	
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
