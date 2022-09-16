40807e69d7260fa0ecb4712d675283f9faf1db6e
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-15 19:55:57 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,15 +2,14 @@
 #include "main.h"
 
 /**
- * print_number - a function that prints an integer.
- * @n: An input integer
- * Return: Nothing
+ * print_number - Prints a number
+ * @n: The number to print
  */
 
 void print_number(int n)
 {
 	unsigned int num = n;
-	
+
 	if (n < 0)
 	{
 		putchar('-');
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-int _putchar(char c);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+int _putchar(char);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
