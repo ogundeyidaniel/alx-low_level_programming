ef907a51afdbc3fd91b886397e5ce6c6a943b4b2
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-15 17:06:27 -0700

    Task 12

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,19 +3,18 @@
 
 #include <stdio.h>
 
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+int _putchar(char c);
 void print_numbers(void);
-void print_most_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void num_to_char(unsigned int);
 void print_number(int n);
-void print_integer(int m);
 
 #endif /* MAIN_H */
