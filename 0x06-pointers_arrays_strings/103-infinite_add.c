472619b8fb828646ffec2ce3aadd7ac2b2aa396d
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-21 17:10:01 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,6 +13,7 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+
 	while (*(n1 + c1) != '\0')
 		c1++;
 	while (*(n2 + c2) != '\0')
@@ -34,6 +35,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			add = 0;
 		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
 			c1--, dr1 = *(n1 + c1) - 48;
 		else
 			dr1 = 0;
