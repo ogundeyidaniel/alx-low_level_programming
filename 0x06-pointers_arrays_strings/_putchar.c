60e49d46c1edaabe17d9cbcc4ff5effcb03989ed
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-09-30 16:32:08 -0700

    Concatenates two strings

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,34 +1,12 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
- * print_number - function to print int using _putchar
- * @n: input integer.
- * Return: no return.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void print_number(int n)
+int _putchar(char c)
 {
-	unsigned int m, d, count;
-
-	if (n < 0)
-	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
-	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	return (write(1, &c, 1));
 }
