0ef886c8478ec73bbaaeb15d07ff79a2e8e7383d
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-10-10 06:03:48 -0700

    Task 2

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
@@ -10,9 +10,9 @@ int main(void)
 {
 	struct dog my_dog;
 
-	my_dog.name = "Django";
+	my_dog.name = "Poppy";
 	my_dog.age = 3.5;
-	my_dog.owner = "Jay";
+	my_dog.owner = "Bob";
 	print_dog(&my_dog);
 	return (0);
 }
