354ac4f480307cc0f1d70600ae10f8258018f1a1
ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-10-12 06:36:17 -0700

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,10 +5,10 @@
  * @array: input integer array.
  * @size: size of the array.
  * @cmp: pointer to the function to be used
- * 		to compare values.
- * Return: the index of the first eement for which the cmp function 
- * 		does not return 0, If no element matches, return -1,
- * 	If size <= 0, return -1.
+ * to compare values.
+ * Return: the index of the first eement for which the cmp function
+ * does not return 0, If no element matches, return -1
+ * If size <= 0, return -1.
  */
 
 int int_index(int *array, int size, int (*cmp)(int));
